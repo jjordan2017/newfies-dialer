7a54ed6429fea4e40ec4d1de081fda10f3fc2deb
James Jordan <jjordan@e-teleco.com>
2024-03-15 15:17:27 -0700

    *change python-dev to python-dev-is-python3 *change libtiff4-dev to libtiff-dev

--- install/install-freeswitch.sh
+++ install/install-freeswitch.sh
@@ -83,13 +83,13 @@ func_install_deps() {
             apt-get -y install unzip zip sox sqlite3 ncftp nmap
             apt-get -y install autoconf2.64 automake autotools-dev binutils bison build-essential cpp curl flex gcc libaudiofile-dev libc6-dev libexpat1 libexpat1-dev mcrypt libmcrypt-dev libnewt-dev libpopt-dev libsctp-dev libx11-dev libxml2 libxml2-dev lksctp-tools lynx m4 openssl ssl-cert zlib1g-dev
 
-            apt-get -y install autoconf automake devscripts gawk g++ git-core 'libjpeg-dev|libjpeg62-turbo-dev' libncurses5-dev 'libtool-bin|libtool' make python-dev gawk pkg-config libtiff5-dev libperl-dev libgdbm-dev libdb-dev gettext libssl-dev libcurl4-openssl-dev libpcre3-dev libspeex-dev libspeexdsp-dev libsqlite3-dev libedit-dev libldns-dev libpq-dev libmp3lame-dev
+            apt-get -y install autoconf automake devscripts gawk g++ git-core 'libjpeg-dev|libjpeg62-turbo-dev' libncurses5-dev 'libtool-bin|libtool' make python-dev-is-python3 gawk pkg-config libtiff5-dev libperl-dev libgdbm-dev libdb-dev gettext libssl-dev libcurl4-openssl-dev libpcre3-dev libspeex-dev libspeexdsp-dev libsqlite3-dev libedit-dev libldns-dev libpq-dev libmp3lame-dev
 
-            if [ $DEBIANCODE != "jessie" ]; then
-                #DEBIAN7
-                apt-get -y install libgnutls-dev libtiff4-dev libtiff4
+            if [ $DEBIANCODE != "bullseye" ]; then
+                #DEBIAN11
+                apt-get -y install libgnutls-dev libtiff-dev libtiff4
             else
-                #DEBIAN8
+                #DEBIAN12
                 apt-get -y install libgnutls28-dev libtiff5-dev libtiff5
             fi
             apt-get -y install libvorbis0a libogg0 libogg-dev libvorbis-dev
