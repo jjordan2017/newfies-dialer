6417ddf82f93382e2f5aa8d9381fec3f0f628b77
James Jordan <jjordan@e-teleco.com>
2024-03-15 14:07:29 -0700

    updated debian install version

--- install/install-all.sh
+++ install/install-all.sh
@@ -47,7 +47,7 @@ func_identify_os() {
     if [ -f /etc/debian_version ] ; then
         DIST='DEBIAN'
         apt-get -y install lsb-release
-        if [ "$(lsb_release -cs)" != "wheezy" ] && [ "$(lsb_release -cs)" != "jessie" ]; then
+        if [ "$(lsb_release -cs)" != "bullseye" ] && [ "$(lsb_release -cs)" != "bookworm" ]; then
             echo $SCRIPT_NOTICE
             exit 255
         fi
